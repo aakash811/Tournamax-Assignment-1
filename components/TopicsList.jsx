9c05ce69ed938489b2f6d886129946b16bffc218
aakash811 <borseaakash082004@gmail.com>
2024-08-15 19:17:08 +0530

    minor changes

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -43,7 +43,7 @@ export default async function TopicsList() {
           return <TopicItem key={_id} topic={topic} />;
         })
       ) : (
-        <p>No topics available</p>
+        <p className="text-center">No topics available</p>
       )}
     </>
   );
