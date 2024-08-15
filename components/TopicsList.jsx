2764adf90aca9321d579bdf0acb9f0d13d31215c
aakash811 <borseaakash082004@gmail.com>
2024-08-15 16:06:10 +0530

    fix

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,22 +1,24 @@
 import TopicItem from "./TopicItem";
 
-const getTopics = async () => {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/topics`, {
-      cache: "no-store",
-    });
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch topics");
-    }
-
-    return res.json();
-  } catch (error) {
-    console.log("Error loading topics: ", error);
-  }
-};
-
 export default async function TopicsList() {
+  const getTopics = async () => {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/topics`, {
+        cache: "no-store",
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch topics");
+      }
+
+      const data = await res.json();
+      return { topics: data.topics || [] };
+    } catch (error) {
+      console.log("Error loading topics: ", error);
+      return { topics: [] };
+    }
+  };
+
   const { topics } = await getTopics();
 
   return (
