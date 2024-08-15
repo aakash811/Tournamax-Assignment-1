a63bfab7207c3d685b128fa9a774db0e2f37b405
aakash811 <borseaakash082004@gmail.com>
2024-08-15 17:02:04 +0530

    fix

--- components/TopicItem.jsx
+++ components/TopicItem.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
-import { HiArrowUp, HiPencilAlt } from "react-icons/hi";
+import { HiPencilAlt } from "react-icons/hi";
 
 export default function TopicItem({ topic }) {
   const [isHovered, setIsHovered] = useState(false);
